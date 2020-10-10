fcda1ffbbac3bd2ce87fec99fdf45d556fe2e31a
Luca Porto <64616137+LucaPort0@users.noreply.github.com>
2020-10-09 22:45:40 -0300

    Updated Final Version

--- FindMaxVal/maxval.c
+++ FindMaxVal/maxval.c
@@ -15,7 +15,7 @@ int main(int argc,char **argv){
     Não modifique este trecho de código
     *************************************************************************************
     */
-	int *vetor, i, maior, tam, localMaior;
+	int *vetor, i, maior, tam;
 
     if ( argc  != 2)
     {
@@ -37,71 +37,63 @@ int main(int argc,char **argv){
     int ini, end; // Variáveis de controle para inicio e fim dos blocos em cada thread
     int threadNum; // Número de thread
     int remainder = tam%T; // Porção remanescente(resto) do vetor
+    int localMaior; // Variável que armazenará o maior local de cada thread
 
-	wtime = omp_get_wtime();
-
-    // Preenchendo vetor
-    #pragma omp parallel num_threads(T) shared(vetor) private(i, ini, end, threadNum)
-    {
-
-    threadNum = omp_get_thread_num(); // Número de thread
-
-    ini = (tam/T) * threadNum; 
-     
-    if(threadNum == T-1){ // Tratamento para o resto, colocando na última thread
-        end = (tam/T)* (threadNum +1) + remainder;    
-    }else{
-        end = (tam/T)* (threadNum +1);
-    }
-
-	// iniciando vetor e fixando o maior valor para validacao
-	for(i = ini; i < end; i++)
-		vetor[i] = 1;
-    }
-
-    #pragma omp barrier // Barreira de sincronização
-    
-
-	vetor[tam/2] = tam; // Inserindo maior valor na metade do vetor
-	maior = 0; // Inicializando um maior global
-
-    int j;
-    /*
-    for(j = 0; j < tam; j++){
-        printf("%d ", vetor[j]);
-    }
-    */
+    // Definindo lock
     omp_lock_t mylock;
 	omp_init_lock(&mylock);
 
-    // Buscando maior valor
-    #pragma omp parallel num_threads(T) shared(vetor, maior) private(i, ini, end, threadNum, localMaior)
+    wtime = omp_get_wtime(); // Inicializando contador de tempo
+
+    maior = 0; // Inicializando um maior global
+
+    // Preenchendo vetor
+    #pragma omp parallel num_threads(T) shared(vetor, maior, tam) private(i, ini, end, threadNum, localMaior)
     {
-    
-    localMaior = 0;
 
-    threadNum = omp_get_thread_num(); // Número de thread
+    threadNum = omp_get_thread_num(); // Número da thread
 
-    ini = (tam/T) * threadNum; 
+    ini = (tam/T) * threadNum; // Define o inicio de cada bloco em cada thread
      
-    if(threadNum == T-1){ // Tratamento para o resto, colocando na última thread
+    if(threadNum == T-1){ // Tratamento para o resto(remainder), colocando-o na última thread
         end = (tam/T)* (threadNum +1) + remainder;    
     }else{
         end = (tam/T)* (threadNum +1);
     }
 
+	// iniciando vetor com nrs "1"
+	for(i = ini; i < end; i++){
+		vetor[i] = 1;
+    }
+
+	vetor[tam/2] = tam; // Inserindo maior valor na metade do vetor
+    localMaior = 0;
+
+    ini = (tam/T) * threadNum; // Define o inicio de cada bloco em cada thread
+     
+    if(threadNum == T-1){ // Tratamento para o resto(remainder), colocando na última thread
+        end = (tam/T)* (threadNum +1) + remainder;    
+    }else{
+        end = (tam/T)* (threadNum +1);
+    }
+
+    // Buscando maior valor
 	for(i = ini; i < end; i++)
 	{
 		if(vetor[i] > localMaior)
 			localMaior = vetor[i];
-            //printf("\n%d %d", localMaior, threadNum);
     }
+
+    // Lock utilizado para a variavel "maior" receber o valor correto
     omp_set_lock(&mylock);
 		if (localMaior > maior)
 			maior = localMaior;
 	omp_unset_lock(&mylock);
-	} // fim do for e da regiao paralela
+
+	} // Fim da regiao paralela
     
+
+
 	/*
 	*************************************************************************************
 	Não modifique este trecho de código
